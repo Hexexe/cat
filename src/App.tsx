4363e3ab28aeea77d6627e977a0296153029aa2b
Hexexe <Tusernameaken@gmail.com>
2022-04-19 20:54:16 +0300

    upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./App.css";
+import "./styles.css";
+import LoadingSpinner from "./components/Loading";
 
 function App() {
   const [cat, setCat] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleClick = async () => {
     try {
+      setLoading(true);
       const catRes = await axios.get("https://aws.random.cat/meow");
       setCat(catRes.data.file);
     } catch (error) {
-      // nope
+      setLoading(false);
     }
   };
 
@@ -18,10 +21,11 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="imageC">
-          <img src={cat} alt="cat" />
+          {loading && <LoadingSpinner /> }
+          <img src={cat} alt="cat" onLoad={() => setLoading(false)} style={{ display: loading ? "none" : "block" }} />
         </div>
         <div className="buttonDiv">
-          <button type="button" onClick={handleClick}>
+          <button type="button" onClick={handleClick} disabled={loading} style={{ display: loading ? "none" : "block" }}>
             {cat ? "Next Cat" : "Get Cat"}
           </button>
         </div>
